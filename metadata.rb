6e0a374cb701e2e896d32efe1d8d3315cf710a35
Tim Smith <tsmith84@gmail.com>
2015-09-08 21:42:05 -0700

    Add long_description and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,15 @@
 name             'fieri'
 maintainer       'Chef Supermarket Team'
-maintainer_email 'supermarket@getchef.com'
+maintainer_email 'supermarket@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures fieri'
-long_description 'Installs/Configures fieri'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
 depends          'apt'
 depends          'git'
 depends          'redisio'
 depends          'xml'
+
+source_url 'https://github.com/chef-cookbooks/fieri' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/fieri/issues' if respond_to?(:issues_url)
